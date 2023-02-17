0fdccb3db2651f8ee9f2736c689f0cb313d2182d
Cici <113744126+Cici-Han@users.noreply.github.com>
2023-02-17 09:49:52 +0000

    modified:   06-Modal/starter/script.js 	modified:   07-Pig-Game/starter/script.js 	modified:   07-Pig-Game/starter/style.css

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+//selecting elements
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  //1.generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //2. display dice
+  diceEL.classList.remove('hidden');
+  diceEL.
+
+  //3. check if rolled 1--true, switch to other player
+});
